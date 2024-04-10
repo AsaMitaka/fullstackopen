2216356c7dfdd77c45a1464f6d80097fa8a2b728
asaMitaka <cepelits@gmail.com>
2024-04-11 01:40:13 +0300

    part4 models

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
+});
+
+module.exports = mongoose.model('User', userSchema);
