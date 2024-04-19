7376d4987d05e29b6af19c71e580af7cc7befd05
asaMitaka <cepelits@gmail.com>
2024-04-20 01:48:34 +0300

    test graphql without db and client

---
+++ test-graphql/server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema');
+
+const PORT = 3000;
+const app = express();
+const users = [{ id: '1', username: 'user1', age: 21 }];
+
+app.use(cors());
+
+const createUser = (input) => {
+  const id = Date.now();
+
+  return {
+    id,
+    ...input,
+  };
+};
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+
+  getUser: ({ id }) => {
+    const user = users.find((user) => user.id === id);
+
+    return user;
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+
+    users.push(user);
+    return user;
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+  }),
+);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
